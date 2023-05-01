b9ea3e0215178b899570fb15c84a16a0e4b87ff4
Juan Manuel Betancur <juanbetancur142474@correo.itm.edu.co>
2023-05-01 15:44:41 -0500

    Creacion del la api y la pagina de periodo academico

--- WebAppSchoolEase/Program.cs
+++ WebAppSchoolEase/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebAppSchoolEase;
+using WebAppSchoolEase.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiSchoolEase = builder.Configuration.GetValue<string>("apiSchoolEase");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiSchoolEase) });
+builder.Services.AddScoped<IPeriodoAcademicoService, PeriodoAcademicoService>();
+
 await builder.Build().RunAsync();
---
+++ WebAppSchoolEase/Services/PeriodoAcademicoService.cs
@@ -0,0 +1,45 @@
+﻿using WebAppSchoolEase.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WebAppSchoolEase.Services
+{
+    public class PeriodoAcademicoService : IPeriodoAcademicoService
+    {
+        private readonly HttpClient client;
+
+        private readonly JsonSerializerOptions options;
+        public PeriodoAcademicoService(HttpClient httpClient)
+        {
+            
+            client = httpClient;
+            options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true};
+        }
+
+        public async Task<List<PeriodoAcademico>?> Get()
+        {
+            var response = await client.GetAsync("api/periodoacademico");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+            return JsonSerializer.Deserialize<List<PeriodoAcademico>>(content, options);
+            
+        }
+        public async Task Add(PeriodoAcademico periodoAcademico)
+        {
+            var response = await client.PostAsync("api/periodoacademico", JsonContent.Create(periodoAcademico));
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+    }
+    public interface IPeriodoAcademicoService
+    {
+        Task<List<PeriodoAcademico>?> Get();
+        Task Add(PeriodoAcademico periodoAcademico);
+    }
+}
