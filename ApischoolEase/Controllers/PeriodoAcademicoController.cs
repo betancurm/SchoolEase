b362f27c9b509c9735b5e7a9677fd4df35204c7b
Juan Manuel Betancur <juanbetancur142474@correo.itm.edu.co>
2023-05-01 15:43:27 -0500

    Creacion del la api y la pagina de periodo academico

---
+++ ApischoolEase/Controllers/PeriodoAcademicoController.cs
@@ -0,0 +1,41 @@
+﻿
+using ApischoolEase.Models;
+using ApischoolEase.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApischoolEase.Controllers
+{
+    [Route("api/[controller]")]
+    public class PeriodoAcademicoController : ControllerBase
+    {
+        IPeriodoAcademicoService periodoAcademicoService;
+        public PeriodoAcademicoController(IPeriodoAcademicoService service)
+        {
+            periodoAcademicoService = service;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(periodoAcademicoService.Get());
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] PeriodoAcademico periodoAcademico)
+        {
+            periodoAcademicoService.Save(periodoAcademico);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id ,[FromBody] PeriodoAcademico periodoAcademico)
+        {
+            periodoAcademicoService.Update(id, periodoAcademico);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            periodoAcademicoService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ ApischoolEase/SchoolEaseContext.cs
@@ -0,0 +1,34 @@
+﻿using ApischoolEase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApischoolEase
+{
+    public class SchoolEaseContext :DbContext
+    {
+        public DbSet<PeriodoAcademico> PeriodosAcademicos { get; set; }
+        public SchoolEaseContext(DbContextOptions<SchoolEaseContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            List<PeriodoAcademico> periodoAcademicosInit = new List<PeriodoAcademico>();
+            periodoAcademicosInit.Add(new PeriodoAcademico()
+            {
+                IdPeriodoAcademico = 1,
+                Nombre = "Periodo 2020",
+                FechaInicio = new DateTime(2020, 1, 13),
+                FechaFin = new DateTime(2020, 11, 20),
+                TipoPeriodo = "Calendario A"
+            });
+            modelBuilder.Entity<PeriodoAcademico>(periodoAcademico =>
+            {
+                periodoAcademico.ToTable("PeriodoAcademico");
+                periodoAcademico.HasKey(p => p.IdPeriodoAcademico);
+                periodoAcademico.Property(p => p.Nombre).IsRequired().HasMaxLength(10);
+                periodoAcademico.Property(p => p.FechaInicio).IsRequired();
+                periodoAcademico.Property(p => p.FechaFin).IsRequired();
+                periodoAcademico.Property(p => p.TipoPeriodo).IsRequired();
+                periodoAcademico.HasData(periodoAcademicosInit);
+            });
+        }
+    }
+}
