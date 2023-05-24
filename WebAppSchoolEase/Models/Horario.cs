2d3f1cb82fe6b81e2f102307cfe65edaf93d11c3
Geral <geraldavila-01@hotmail.com>
2023-05-23 23:26:30 -0500

    Insert Horarios

--- WebAppSchoolEase/Models/Horario.cs
+++ WebAppSchoolEase/Models/Horario.cs
@@ -3,7 +3,7 @@
     public class Horario
     {
         public int IdHorario { get; set; }
-        public DateTime DiaSemana { get; set; }
+        public int DiaSemana { get; set; }
         public DateTime HoraInicio { get; set; }
         public DateTime HoraFin { get; set; }
     }
